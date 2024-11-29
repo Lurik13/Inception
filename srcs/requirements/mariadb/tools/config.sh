ad1ed3f1a2228b5c6541222c6dea4365d251b0cb
Lucas Ribette <116387708+Lurik13@users.noreply.github.com>
2024-11-29 17:26:55 +0100

    Wow Zoe helped me a lot with mariadb

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# set -x  
+# set -x
 
-service mariadb start;
+mariadbd-safe &
+sleep 1
+while ! mariadb-check --all-databases
+do 
+    sleep 1
+done
+
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;"
-mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'wordpress.Inception' IDENTIFIED BY '$DB_PASSWORD';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'wordpress.Inception' IDENTIFIED BY '$DB_PASSWORD';"
 mariadb -e "FLUSH PRIVILEGES;"
 
-# mariadb-admin -u root password "$DB_ROOT_PASSWORD"
-# mariadb-admin -u root -p"$DB_ROOT_PASSWORD" shutdown
+mariadb-admin -u root password "$DB_ROOT_PASSWORD"
+mariadb-admin -u root -p"$DB_ROOT_PASSWORD" shutdown
 
-# mariadbd-safe;
+exec mariadbd-safe
--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ ! -f "/var/www/html/wp-config.php"]; then
+if [ ! -f "/var/www/html/wp-config.php" ] 
+then
     echo "\e[38;2;0;120;200mConfiguring WordPress...\e[0m"
 else
     echo "\e[38;2;120;200;0mWordPress is already configured.\e[0m"
@@ -17,17 +18,14 @@ fi
                                     --dbname=$DB_NAME \
                                     --dbuser=$DB_USER \
                                     --dbpass=$DB_PASSWORD \
-                                    --dbhost=$DB_HOST \
-                                    --path='/var/www/html'
+                                    --dbhost=$DB_HOST
 
     wp core install --allow-root \
                     --url=$WP_URL \
                     --title=$WP_TITLE \
                     --admin_user=$WP_ADMIN_USER \
                     --admin_password=$WP_ADMIN_PASSWORD \
-                    --admin_email=$WP_ADMIN_EMAIL
+                    --admin_email=$WP_ADMIN_EMAIL \
+                    --skip-email
 
-echo "\e[48;2;0;0;0m------------------------------------\e[0m"
-wp --info
-echo "\e[48;2;200;0;0m------------------------------------\e[0m"
-cd /var/www ; ls *
+
