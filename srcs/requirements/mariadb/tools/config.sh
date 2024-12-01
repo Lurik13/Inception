c78512479241a3f05a4a6cd34125134c54537b04
Lucas Ribette <116387708+Lurik13@users.noreply.github.com>
2024-12-01 17:27:07 +0100

    trying to fix that php-fpm of hell

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -9,10 +9,11 @@ do
     sleep 1
 done
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;"
-mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'wordpress.Inception' IDENTIFIED BY '$DB_PASSWORD';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'wordpress.Inception' IDENTIFIED BY '$DB_PASSWORD';"
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb --execute "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;"
+mariadb --execute "CREATE USER IF NOT EXISTS '$DB_USER'@'wordpress.Inception' IDENTIFIED BY '$DB_PASSWORD';"
+mariadb --execute "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'wordpress.Inception' IDENTIFIED BY '$DB_PASSWORD';"
+# Reload the privilege tables to apply changes
+mariadb --execute "FLUSH PRIVILEGES;"
 
 mariadb-admin -u root password "$DB_ROOT_PASSWORD"
 mariadb-admin -u root -p"$DB_ROOT_PASSWORD" shutdown
--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -2,15 +2,11 @@
 
 if [ ! -f "/var/www/html/wp-config.php" ] 
 then
+    # rm /etc/php/7.4/fpm/pool.d/www.conf
+    service php7.4-fpm start
+    systemctl enable php7.4-fpm.service
+    systemctl start php7.4-fpm.service
     echo "\e[38;2;0;120;200mConfiguring WordPress...\e[0m"
-else
-    echo "\e[38;2;120;200;0mWordPress is already configured.\e[0m"
-fi
-
-# wp config
-# wp core
-# wp user
-# wp options
 
     wp core download --allow-root --path='/var/www/html' --locale=fr_FR
 
@@ -28,4 +24,21 @@ fi
                     --admin_email=$WP_ADMIN_EMAIL \
                     --skip-email
 
+else
+    echo "\e[38;2;120;200;0mWordPress is already configured.\e[0m"
+fi
 
+# sudo systemctl restart php7.4-fpm.service
+# sudo systemctl enable php7.4-fpm.service
+echo "\e[38;2;255;120;200mphp-fpm en-dessous:\e[0m"
+service php7.4-fpm status
+systemctl status php7.4-fpm
+echo "\e[38;2;255;120;200mphp-fpm au-dessus:\e[0m"
+# while true ; do echo "" ; done
+# systemctl status php7.4-fpm 
+# /usr/sbin/php-fpm7.4 -F
+exec "$@"
+# wp config
+# wp core
+# wp user
+# wp options
