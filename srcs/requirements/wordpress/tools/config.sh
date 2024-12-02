c4d4b066649b8c2ebeb0b72ba965fbdbfd6569c2
Lucas Ribette <116387708+Lurik13@users.noreply.github.com>
2024-12-02 19:18:31 +0100

    No errors! Can load my cv with the 3 containers

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -3,9 +3,7 @@
 if [ ! -f "/var/www/html/wp-config.php" ] 
 then
     # rm /etc/php/7.4/fpm/pool.d/www.conf
-    systemctl enable php7.4-fpm.service
-    systemctl start php7.4-fpm.service
-    service php7.4-fpm start
+    # service php7.4-fpm start
     echo "\e[38;2;0;120;200mConfiguring WordPress...\e[0m"
 
     wp core download --allow-root --path='/var/www/html' --locale=fr_FR
@@ -28,13 +26,24 @@ else
     echo "\e[38;2;120;200;0mWordPress is already configured.\e[0m"
 fi
 
-echo "\e[38;2;255;120;200mphp-fpm en-dessous:\e[0m"
+# echo "\e[38;2;255;120;200mphp-fpm en-dessous:\e[0m"
 # service php7.4-fpm status
 # systemctl status php7.4-fpm
-echo "\e[38;2;255;120;200mphp-fpm au-dessus:\e[0m"
+# echo "\e[38;2;255;120;200mphp-fpm au-dessus:\e[0m"
 # while true ; do echo "" ; done
 # exec "$@"
 exec /usr/sbin/php-fpm7.4 -F
+
+
+
+
+
+
+
+
+
+
+
 # wp config
 # wp core
 # wp user
