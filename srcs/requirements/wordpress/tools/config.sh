9e2167de12756554d6f78c8e8c584d9a56257453
Lucas Ribette <116387708+Lurik13@users.noreply.github.com>
2024-12-03 20:19:27 +0100

    It works!

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -2,8 +2,6 @@
 
 if [ ! -f "/var/www/wordpress/wp-config.php" ] 
 then
-    # rm /etc/php/7.4/fpm/pool.d/www.conf
-    # service php7.4-fpm start
     echo "\e[38;2;0;120;200mConfiguring WordPress...\e[0m"
 
     wp core download --allow-root --path='/var/www/wordpress' --locale=fr_FR
@@ -21,36 +19,17 @@ then
                     --admin_password=$WP_ADMIN_PASSWORD \
                     --admin_email=$WP_ADMIN_EMAIL \
                     --skip-email
+
+    wp user create  $WP_USER_LOGIN \
+                    $WP_USER_EMAIL \
+                    --role=subscriber \
+                    --user_pass=$WP_USER_PASSWORD \
+                    --allow-root
+    
     chown -R www-data:www-data .
-    # wp package install curl --allow-root
-    # wp package install dom --allow-root
-    # wp package install imagick --allow-root
-    # wp package install zip --allow-root
-    # wp package install gd --allow-root
-    # wp package install intl --allow-root
+
 else
     echo "\e[38;2;120;200;0mWordPress is already configured.\e[0m"
 fi
 
-# echo "\e[38;2;255;120;200mphp-fpm en-dessous:\e[0m"
-# service php7.4-fpm status
-# systemctl status php7.4-fpm
-# echo "\e[38;2;255;120;200mphp-fpm au-dessus:\e[0m"
-# while true ; do echo "" ; done
-# exec "$@"
 exec /usr/sbin/php-fpm7.4 -F
-
-
-
-
-
-
-
-
-
-
-
-# wp config
-# wp core
-# wp user
-# wp options
